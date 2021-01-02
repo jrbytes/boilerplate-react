ec76c1c47dceed9ab16e3cf3964cf6d568117318
Jr Bytes <itamazor@gmail.com>
2021-01-01 22:36:37 -0300

    Added theme provider.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: ["../src/components/**/stories.tsx"],
   addons: ["@storybook/addon-essentials"],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  },
   babel: async (options) => ({
     ...options,
     plugins: [...options.plugins, '@babel/plugin-transform-react-jsx']
